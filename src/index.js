3f01929efaee6e68f9a397e5e2af80d7f9934581
Marina Lugovskaya <anomaliya1993@gmail.com>
2021-07-10 00:56:03 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import './sass/main.scss';
+
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+}
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.timerId = null;
+  }
+
+  start() {
+        const startTime = Date.now();
+
+        this.intervalId = setInterval(() => {
+          const currentTime = Date.now();
+          const deltaTime = this.targetDate - Date.now();
+          const time = this.getTimeComponents(deltaTime);
+        },1000);
+  }
+  getTimeComponents(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  
+    return {days, hours, mins, secs};
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+  const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+  });
+
+  
+  function updateClockFace({days, hours, mins, secs}) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+
+  function countdownTimer() {
+    if (deltaTime  <= 0) {
+      clearInterval(timerId);
+    }
+  }
